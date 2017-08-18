680d0108ad9a051a4a6dc3ce45df626d00c4f93f
Noel Quiles <contact@noelquiles.com>
2017-08-18 17:52:28 -0400

    Update index.js

--- test/index.js
+++ test/index.js
@@ -1,31 +1,24 @@
 const beautify = require('..')
 const path = require('path')
+const sugarml = require('sugarml')
 const {readFileSync} = require('fs')
 const reshape = require('reshape')
 const test = require('ava')
 const fixtures = path.join(__dirname, 'fixtures')
 
-test('basic', (t) => {
-  return compare(t, 'basic')
+test('line eating test (with surrounding tags)', (t) => {
+  return compare(t, 'linestrip-surrounding-tags')
 })
 
-test('strips and replaces existing indentation', (t) => {
-  return compare(t, 'bad_indents')
-})
-
-test('self-closing tags', (t) => {
-  return compare(t, 'selfclosing')
-})
-
-test('custom indent level', (t) => {
-  return compare(t, 'custom_indent', { indent: 4 })
+test('line eating test (with only lines)', (t) => {
+  return compare(t, 'linestrip-lines-only')
 })
 
 function compare (t, name, opts) {
-  const src = readFileSync(path.join(fixtures, `${name}.html`), 'utf8')
+  const src = readFileSync(path.join(fixtures, `${name}.sgr`), 'utf8')
   const expected = readFileSync(path.join(fixtures, `${name}.expected.html`), 'utf8')
 
-  return reshape({ plugins: beautify(opts) })
+  return reshape({ parser: sugarml, plugins: beautify(opts) })
     .process(src)
     .then((res) => res.output())
     .tap((out) => t.is(expected, out))
